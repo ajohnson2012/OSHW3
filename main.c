6fb1fe9bc56f1691be2ab44aa515b03ef7e4c9ad
apj4b <apj4b@virginia.edu>
2011-11-20 18:50:33 -0800

    AWWW SHIT SON I GOT ALL THIS TO WORK MUHAHAHAHHA....not

--- main.c
+++ main.c
@@ -14,7 +14,6 @@ int main (int argc, char *argv[]){
 	homeName= getenv("HOME");
 	pid_t child_pid;
 	int child_status;
-	printf("TEST");
 	while (flag){
 		if (myShellName == NULL)
 			printf("mysh$ ");
@@ -37,22 +36,26 @@ int main (int argc, char *argv[]){
 
 
 int runcmd(char *cmd){
-	pid_t child_pid;	
+	pid_t child_pid;
+	int child_status;	
 	if ((child_pid=vfork()) == 0) {
 		//In child
 		if (execvp(args[0], args) ==-1){
-			printf("GREAT GOLLY MISS MOLLY THERE WAS AN ERROR");
+			printf("GREAT GOLLY MISS MOLLY THERE WAS AN ERROR \n");
 		}
+		_exit(0);
 	}
 	else { 
 	//In parent
+		wait(&child_status);
+		printf("AWWW SHIT SON IM THE BIG DADDY PROCESS \n");
 		/*do{
-		//	pid_t tpid=wait(&child_status);
-		//	if(tpid!=child_pid) {
-			//	process_terminated(tpid);
+			pid_t tpid=wait(&child_status);
+			if(tpid!=child_pid) {
+				process_terminated(tpid);
 			}
-		}while(tpid!=child_pid);	
-*/	
+		}while(tpid!=child_pid);	*/
+	
 	}
 }
 
